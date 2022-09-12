5f993e0ad0da687344be193752cb7d4ae27427be
alinekabbas <alinekabbas@gmail.com>
2022-09-12 16:04:55 -0300

    Exercicios corrigidos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,15 +1,17 @@
 //Utilizando o laço `while`, escreva um código que recebe um `prompt` perguntando se a pessoa deseja comer mais coxinhas. 
 
-let pergunta = prompt("Você deseja comer mais coxinhas? \nResponda: \nS para Sim ou \N para Não")
+let pergunta = prompt("Você deseja comer mais coxinhas? \nResponda: \nS para Sim ou \N para Não").toUpperCase()
 
 //Crie também uma `let conta` para guardar o valor total.
-let valor = 2.5
 let conta = 2.5
+let valorCoxinha= 2.5
 
 //Toda vez que a resposta for **sim**, o laço deve se repetir, e devemos adicionar R$2.50 ao valor total. Quando a resposta não for **sim**, devemos imprimir o valor total da conta.
 
+
 while(pergunta === "S"){
-    conta = conta + valor
-    console.log(conta)
+   conta = conta + valorCoxinha
+   pergunta = prompt("Você deseja comer mais coxinhas? \nResponda: \nS para Sim ou \N para Não").toUpperCase()
 }
-console.log("Obrigada pela sua compra")
+console.log(`O valor ficou em R$ ${conta}.`)
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,6 +1,10 @@
-const ordemCrepusculo = ["Crepúsculo", "Lua Nova", "Eclipse", "Amanhecer - Parte 1", "Amanhecer - Parte 2"]
+//Crie um array com 5 strings.
 
-for(let i = 0; i < ordemCrepusculo.length; i++){
+const ordemCrepusculo = ["1 - Crepúsculo", "2 - Lua Nova", "3 - Eclipse", "4 - Amanhecer - Parte 1", "5 - Amanhecer - Parte 2"]
+
+//Faça um programa que percorra este array e imprima as strings em formato de ranking
+
+for(let i = 0; i < ordemCrepusculo.length   ; i++){
     console.log(ordemCrepusculo[i])
 }
 
